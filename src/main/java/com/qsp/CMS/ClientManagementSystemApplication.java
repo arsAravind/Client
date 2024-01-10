c44e0b80fef04097edc0616fcdbddd83ca69c510
arsAravind <saravindan7871@gmail.com>
2024-01-10 20:01:35 +0530

    jhbdfj

--- src/main/java/com/qsp/CMS/ClientManagementSystemApplication.java
+++ src/main/java/com/qsp/CMS/ClientManagementSystemApplication.java
@@ -8,6 +8,7 @@ public class ClientManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientManagementSystemApplication.class, args);
+		//spring
 	}
 
 }
